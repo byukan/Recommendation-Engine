6b37dec8ee21961791c487804caf3ef0a0ca8c30
Justin J. Wang <justwjr@ucla.edu>
2017-04-15 18:58:04 -0700

    first commit

---
+++ MyRecommendation/MyRecommendation/src/main/scala/Serving.scala
@@ -0,0 +1,13 @@
+package recommender
+
+import org.apache.predictionio.controller.LServing
+
+class Serving
+  extends LServing[Query, PredictedResult] {
+
+  override
+  def serve(query: Query,
+    predictedResults: Seq[PredictedResult]): PredictedResult = {
+    predictedResults.head
+  }
+}
